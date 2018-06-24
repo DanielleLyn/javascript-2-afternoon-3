da81d07f7fffbd510d5b4d9cf9e136f549c605c9
Danielle Cucinotta <Danielle.cucinotta@gmail.com>
2018-06-23 18:18:20 -0700

    5 failures

--- practice.js
+++ practice.js
@@ -28,7 +28,9 @@
 */
 
 // Code Here 
-
+function first(array,callback){
+  return callback(array[0]);
+}
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
@@ -48,7 +50,11 @@ first(names, function(firstName){
 */
 
 //Code Here
-
+function last(array,callback){
+  for(var i=array.length-1;i=array.length-1;i++){
+    return callback(array[i]);
+  }
+}
 // Do not edit the code below.
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
@@ -66,7 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
-
+function multiply(num1,num2,callback){
+  return callback(num1*num2);
+}
 // Do not edit the code below.
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -85,7 +93,15 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
-
+function contains(array,name,callback){
+  for(var i=0;i<array.length;i++){
+    if(array[i]===name){
+      return callback(true);
+    } else{
+      return callback(false);
+    }
+  }
+}
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
   if(result === true){
@@ -106,7 +122,14 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
-
+function uniq (array,callback){
+  for(var i=array.length-1;i>0;i--){
+    if(array[i]===array[i-1]){
+      array.splice(i,1);
+    }
+    return callback(array);
+  }
+}
 // Do not edit the code below.
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -123,7 +146,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
-
+function each(array,callback){
+  for (var i=0;i<array.length;i++){
+    return callback(array[i], array.indexOf[i]);
+  }
+}
 // Do not edit the code below.
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
@@ -140,7 +167,13 @@ each(names, function(item, indice){
 */
 
 // Code here
-
+function getUserById(users,id,callback){
+  for(var i=0;i<users.length;i++){
+    if (users[i]===id){
+      return callback(users[i]);
+    }
+  }
+}
 // Do not edit the code below.
 var users = [
   {
